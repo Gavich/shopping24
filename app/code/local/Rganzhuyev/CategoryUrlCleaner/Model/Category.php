464f96d2c6a8d07f0b0e59a2af0dc2417f84ca2d
Ruslan <gangrus@yandex.ru>
2013-06-19 17:40:03 +0300

    Category url

--- app/code/local/Rganzhuyev/CategoryUrlCleaner/Model/Category.php
+++ app/code/local/Rganzhuyev/CategoryUrlCleaner/Model/Category.php
@@ -5,7 +5,8 @@ class Rganzhuyev_CategoryUrlCleaner_Model_Category extends Mage_Catalog_Model_Ca
         $parentUrl = parent::getUrl();
         $urlExploded = explode('/', $parentUrl);
         $url =  array_pop($urlExploded);
-        $url = Mage::getUrl('').$this->getId().'-category-'.$url;
+        $url = 'c'.str_replace('.html', '', $this->getId().'-'.$url);
+        $url = Mage::getUrl($url);
 //        $url = str_replace(Mage::getUrl(''), Mage::getUrl('').$this->getId().'-category-', $parentUrl);
         return $url;
     }
--- app/code/local/Rganzhuyev/CategoryUrlCleaner/Model/Observer.php
+++ app/code/local/Rganzhuyev/CategoryUrlCleaner/Model/Observer.php
@@ -4,16 +4,24 @@ class Rganzhuyev_CategoryUrlCleaner_Model_Observer extends Mage_Catalog_Model_Ob
     public function categoryUrlClean(Varien_Event_Observer $observer){
         $request = Mage::app()->getRequest();
         $url = $request->getOriginalPathInfo();
+        if(substr($url, -1) == '/') {
+            $url = substr($url, 0, strlen($url)-1);
+        }
+//        var_dump($url);
         $path = explode('/', $url);
         $path = array_reverse($path);
         $categoryUrl = explode('-', $path[0]);
-        $categoryId = array_shift($categoryUrl);
-        $isCategory = @array_shift($categoryUrl) == 'category';
+        $category = array_shift($categoryUrl);
+        $isCategory = substr($category, 0,1) == 'c';
+        $categoryId = (int)substr($category, 1);
         $categoryPath = implode('-', $categoryUrl);
         $urlPath = Mage::getModel('core/url_rewrite')->loadByIdPath('category/'.$categoryId);
 
         $urlPath = $urlPath->getData('request_path');
-
+//        var_dump($urlPath);
+//        var_dump($categoryId);
+//        var_dump(strpos($urlPath, $categoryPath));
+//        var_dump($isCategory); var_dump($categoryPath); var_dump($categoryUrl); var_dump($path); die('HERE');
         if ($isCategory && $urlPath && strpos($urlPath, $categoryPath) !== false) {
             $moduleName = 'catalog';
             $controllerName = 'category';
