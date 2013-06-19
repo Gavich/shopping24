c23288c0c43748934d5c5534391b77b1dee5447a
Ruslan <gangrus@yandex.ru>
2013-06-19 13:37:08 +0300

    category url optimization

--- app/code/local/Rganzhuyev/CategoryUrlCleaner/Model/Category.php
+++ app/code/local/Rganzhuyev/CategoryUrlCleaner/Model/Category.php
@@ -3,7 +3,10 @@ class Rganzhuyev_CategoryUrlCleaner_Model_Category extends Mage_Catalog_Model_Ca
 
     public function getUrl(){
         $parentUrl = parent::getUrl();
-        $url = str_replace(Mage::getUrl(''), Mage::getUrl('').$this->getId().'-category-', $parentUrl);
+        $urlExploded = explode('/', $parentUrl);
+        $url =  array_pop($urlExploded);
+        $url = Mage::getUrl('').$this->getId().'-category-'.$url;
+//        $url = str_replace(Mage::getUrl(''), Mage::getUrl('').$this->getId().'-category-', $parentUrl);
         return $url;
     }
 }
