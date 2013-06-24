598b162b2c9306fe63d74f3fb441bd03670135f3
Ruslan <gangrus@yandex.ru>
2013-06-24 12:44:19 +0300

    Category Url cleaner admin enabled option

--- app/code/local/Rganzhuyev/CategoryUrlCleaner/Model/Category.php
+++ app/code/local/Rganzhuyev/CategoryUrlCleaner/Model/Category.php
@@ -2,12 +2,18 @@
 class Rganzhuyev_CategoryUrlCleaner_Model_Category extends Mage_Catalog_Model_Category {
 
     public function getUrl(){
-        $parentUrl = parent::getUrl();
-        $urlExploded = explode('/', $parentUrl);
-        $url =  array_pop($urlExploded);
-        $url = 'c'.str_replace('.html', '', $this->getId().'-'.$url);
-        $url = Mage::getUrl($url);
-//        $url = str_replace(Mage::getUrl(''), Mage::getUrl('').$this->getId().'-category-', $parentUrl);
-        return $url;
+        if (Mage::getStoreConfig('category_url_cleaner/general/enabled'))
+        {
+            $parentUrl = parent::getUrl();
+            $urlExploded = explode('/', $parentUrl);
+            $url =  array_pop($urlExploded);
+            $url = 'c'.str_replace('.html', '', $this->getId().'-'.$url);
+            $url = Mage::getUrl($url);
+    //        $url = str_replace(Mage::getUrl(''), Mage::getUrl('').$this->getId().'-category-', $parentUrl);
+            return $url;
+        } else
+        {
+            return parent::getUrl();
+        }
     }
 }
--- app/code/local/Rganzhuyev/CategoryUrlCleaner/Model/Observer.php
+++ app/code/local/Rganzhuyev/CategoryUrlCleaner/Model/Observer.php
@@ -2,35 +2,35 @@
 class Rganzhuyev_CategoryUrlCleaner_Model_Observer extends Mage_Catalog_Model_Observer {
 
     public function categoryUrlClean(Varien_Event_Observer $observer){
-        $request = Mage::app()->getRequest();
-        $url = $request->getOriginalPathInfo();
-        if(substr($url, -1) == '/') {
-            $url = substr($url, 0, strlen($url)-1);
-        }
-//        var_dump($url);
-        $path = explode('/', $url);
-        $path = array_reverse($path);
-        $categoryUrl = explode('-', $path[0]);
-        $category = array_shift($categoryUrl);
-        $isCategory = substr($category, 0,1) == 'c';
-        $categoryId = (int)substr($category, 1);
-        $categoryPath = implode('-', $categoryUrl);
-        $urlPath = Mage::getModel('core/url_rewrite')->loadByIdPath('category/'.$categoryId);
+        if (Mage::getStoreConfig('category_url_cleaner/general/enabled'))
+        {
+            $request = Mage::app()->getRequest();
+            $url = $request->getOriginalPathInfo();
+            if(substr($url, -1) == '/') {
+                $url = substr($url, 0, strlen($url)-1);
+            }
 
-        $urlPath = $urlPath->getData('request_path');
-//        var_dump($urlPath);
-//        var_dump($categoryId);
-//        var_dump(strpos($urlPath, $categoryPath));
-//        var_dump($isCategory); var_dump($categoryPath); var_dump($categoryUrl); var_dump($path); die('HERE');
-        if ($isCategory && $urlPath && strpos($urlPath, $categoryPath) !== false) {
-            $moduleName = 'catalog';
-            $controllerName = 'category';
-            $actionName = 'view';
-            $request->setModuleName($moduleName)
-                ->setControllerName($controllerName)
-                ->setActionName($actionName)
-                ->setParam('id', $categoryId);
-            $observer->getFront()->setRequest($request);
+            $path = explode('/', $url);
+            $path = array_reverse($path);
+            $categoryUrl = explode('-', $path[0]);
+            $category = array_shift($categoryUrl);
+            $isCategory = substr($category, 0,1) == 'c';
+            $categoryId = (int)substr($category, 1);
+            $categoryPath = implode('-', $categoryUrl);
+            $urlPath = Mage::getModel('core/url_rewrite')->loadByIdPath('category/'.$categoryId);
+
+            $urlPath = $urlPath->getData('request_path');
+
+            if ($isCategory && $urlPath && strpos($urlPath, $categoryPath) !== false) {
+                $moduleName = 'catalog';
+                $controllerName = 'category';
+                $actionName = 'view';
+                $request->setModuleName($moduleName)
+                    ->setControllerName($controllerName)
+                    ->setActionName($actionName)
+                    ->setParam('id', $categoryId);
+                $observer->getFront()->setRequest($request);
+            }
         }
     }
 }
